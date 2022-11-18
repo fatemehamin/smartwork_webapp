fe804dedd2054f11b9cde9ac4f4a7796c48cb90f
F A T E M E H ^~^ <fa.ss501.fa@gmail.com>
2022-11-18 19:24:30 +0330

    add assets and app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+// import 'react-native-gesture-handler';
+// import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+// import Navigation from './src/navigation';
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store/index";
+// import SplashScreen from 'react-native-splash-screen';
+import "./App.css";
+import Login from "./pages/login";
+import Employee from "./pages/employee";
 
-function App() {
+// const MyTheme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     background: '#f5f5f5',
+//   },
+// };
+
+const App = () => {
+  // //Hide Splash screen on app load.
+  // useEffect(() => {
+  //   SplashScreen.hide();
+  // });
   return (
+    // <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Employee />
     </div>
+    // </Provider>
   );
-}
+};
 
 export default App;
